ec3f68f1cc46f035988c28122e3c7629c335040c
Bao Vi Long <vlbao@tma.com.vn>
2024-01-10 18:27:54 +0700

    10/1/24

--- lc/cpp/m131.cpp
+++ lc/cpp/m131.cpp
@@ -30,62 +30,42 @@ class Solution {
                         start++;
                     }
                 }
-                std::cout << "Loop " << j << " \n";
-                for(auto x : result) {
-                    std::cout << "{ ";
-                    for(auto y : x) {
-                        std::cout << y << ",";
-                    }
-                    std::cout << " }\n";  
-                }
-                std::cout << "--------------------\n";
+                // std::cout << "Loop " << j << " \n";
+                // for(auto x : result) {
+                //     std::cout << "{ ";
+                //     for(auto y : x) {
+                //         std::cout << y << ",";
+                //     }
+                //     std::cout << " }\n";  
+                // }
+                // std::cout << "--------------------\n";
             }
             return result;
+        }
+        bool isPalindrome(std::vector<std::vector<bool>> &dp, int i, int j, std::string &s){
+            if(i == j){
+                return dp[i][j] = true;
+            }
+            if(j-i == 1){
+                if(s[i] == s[j]){
+                    return dp[i][j] = true;
+                }
+                else{
+                    return dp[i][j] = false;
+                }
+            }
+            if(s[i] == s[j] && dp[i+1][j-1] == true){
+                return dp[i][j] = true;
+            } else {
+                return dp[i][j] = false;
+            }
+        }
+        std::vector<std::vector<std::string>> get_all_palindrome_sub_string(std::string s) {
+
         }
 };
 
 int main() {
     std::vector<std::vector<std::string>> result;
     std::vector<std::vector<std::string>> a = { {"a","aa","aabaa"}, {"a","aba"}, {"b","baab"}, {"a", "aa"}, {"a"}, {"b"} };
-//     for(auto x : a[0]) {
-//         std::vector<std::string> temp;
-//         temp.push_back(x);
-//         result.push_back(temp);
-//     }
-//     for(int j = 1; j < a.size(); j++) {
-//         int loop = result.size();
-//         int start = 0;
-
-//         while(start < loop) {
-//             int len = 0;
-//             for(auto z : result[start]) {
-//                 len += z.length();
-//             }
-//             if( len <= j) {
-//                 for(auto x : a[j]) {    
-//                     std::vector<std::string> temp = result[start];            
-//                     temp.push_back(x);
-//                     result.push_back(temp);
-//                     }
-//                 result.erase(result.begin() + start);
-//                 loop--;
-//                 }
-//             else {
-//                 start++;
-//             }
-//         }
-//         std::cout << "Loop " << j << " \n";
-//         for(auto x : result) {
-//             std::cout << "{ ";
-//         for(auto y : x) {
-//             std::cout << y << ",";
-//         }
-//         std::cout << " }\n";
-        
-//     }
-//     std::cout << "--------------------\n";
-// }
-    Solution z;
-    z.concat_vector_of_string_vector(a, result);
-
 }
